5bbb1e9825c0877aa4e09f44ef549dd2f3ec28a4
geo <me@gws.io>
2023-03-05 11:55:09 +0800

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Esate',
+    'version': '0.1',
+    'category': '',
+    'sequence': 15,
+    'summary': 'Track real estate',
+    'description': "",
+    'website': 'https://www.uusupply.com/'
+}
